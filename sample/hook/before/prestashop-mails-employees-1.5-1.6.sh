56ff808e96ec11b1b7bd31e4c33ec38aaace703f
Francis GILLES <francis@quai13.com>
2016-11-04 15:43:28 +0100

    ajout table contact

--- sample/hook/before/prestashop-mails-employees-1.5-1.6.sh
+++ sample/hook/before/prestashop-mails-employees-1.5-1.6.sh
@@ -5,6 +5,7 @@ if [ -n "$dst_mail_send" ]; then
     mail_locale=${dst_mail_send%@*}
     mail_domain=${dst_mail_send#*@}
     #MAJ la config des noms de domaine dans la BD
+    echo "UPDATE ps_contact SET email = CONCAT('$mail_locale+', LEFT(email, LOCATE('@', email, 1)), '$mail_domain'); " >> $sqlfile;
     echo "UPDATE ps_employee SET email = CONCAT('$mail_locale+', LEFT(email, LOCATE('@', email, 1)), '$mail_domain'); " >> $sqlfile;
     #affichage OK
     let COL=70-${#MSG}
